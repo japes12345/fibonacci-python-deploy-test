e45a1d3a066e00b5670142e7f55da5f4d1bd8bc4
japes12345 <jpaulmeyer6@gmail.com>
2023-06-15 08:05:23 -0400

    first

---
+++ fibo.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib = [0, 1]
+        for _ in range(2, n):
+            fib.append(fib[-1] + fib[-2])
+        return fib
+
+@app.route('/fibonacci/<int:n>')
+def get_fibonacci(n):
+    fib_seq = fibonacci(n)
+    return jsonify(fib_seq)
+
+if __name__ == '__main__':
+    app.run()
